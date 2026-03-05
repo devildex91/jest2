1d974116c39b8845a62ffbd6bb9442f05e830cfa
Neall Dexter <neall.t.dexter@gmail.com>
2026-03-05 19:41:31 +0000

    tests added to check playerMoves and choices key exists

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+let game = {
+    score:0,
+    currentGame: [],
+    playerMoves:[],
+    choices:[],
+}
+module.exports = {game};
---
+++ scripts/tests/game.test.js
@@ -0,0 +1,24 @@
+/**
+ * @ jest-environment jsdom
+ */
+const {game} = require("../game");
+beforeAll(() =>{
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html","utf-8");
+    document.body.innerHTML = fileContents;
+}); 
+
+describe("game object contains correct keys", ()=> {
+    test("score key exists", ()=>{
+        expect("score" in game).toBe(true);
+    });
+     test("current game key exists", ()=>{
+        expect("currentGame" in game).toBe(true);
+    });
+     test("playerMoves key exists", ()=>{
+        expect("playerMoves" in game).toBe(true);
+    });
+     test("choices key exists", ()=>{
+        expect("choices" in game).toBe(true);
+    });
+});
